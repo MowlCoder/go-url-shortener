443f1d38c11d1af6224c1b7f3dcd6e84b8342f35
MowlCoder <maikezseller@gmail.com>
2023-07-26 19:44:45 +0300

    add test to url storage

---
+++ internal/app/storage/url_test.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestURLStorage_SaveURL(t *testing.T) {
+	t.Run("Save url", func(t *testing.T) {
+		urlToAdd := "https://test.com"
+		storage := NewURLStorage()
+		id, err := storage.SaveURL(urlToAdd)
+
+		if assert.NoError(t, err) {
+			if assert.NotEmpty(t, id) {
+				assert.Equal(t, urlToAdd, storage.structure[id])
+			}
+		}
+	})
+}
+
+func TestURLStorage_GetURLByID(t *testing.T) {
+	t.Run("Get url", func(t *testing.T) {
+		testID := "testid"
+		testURL := "https://test.com"
+		storage := NewURLStorage()
+		storage.structure[testID] = testURL
+
+		url, err := storage.GetURLByID(testID)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, testURL, url)
+		}
+	})
+
+	t.Run("Get not existing url", func(t *testing.T) {
+		testID := "testid"
+		storage := NewURLStorage()
+
+		_, err := storage.GetURLByID(testID)
+
+		assert.Error(t, err)
+	})
+}
