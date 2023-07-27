a76109d14cb74b3047e9539fd4a2c99823439937
MowlCoder <maikezseller@gmail.com>
2023-07-27 09:27:26 +0300

    add chi middlewares

--- internal/app/routes/router.go
+++ internal/app/routes/router.go
@@ -3,10 +3,15 @@ package routes
 import (
 	"github.com/MowlCoder/go-url-shortener/internal/app/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func InitRouter() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Post("/", handlers.ShortURL)
 	r.Get("/{id}", handlers.RedirectToURLByID)
