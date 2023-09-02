22bede3bbc32ac7fea83911231962c4ee9f85cfd
MowlCoder <maikezseller@gmail.com>
2023-09-02 20:04:05 +0100

    fix text version of shorten

--- internal/app/handlers/shortener.go
+++ internal/app/handlers/shortener.go
@@ -151,6 +151,8 @@ func (h *ShortenerHandler) ShortURL(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		fmt.Println(err)
+
 		SendStatusCode(w, http.StatusInternalServerError)
 		return
 	}
