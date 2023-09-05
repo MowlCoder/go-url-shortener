49521516fe7e0e1a438368901d0a43c54723bf79
MowlCoder <maikezseller@gmail.com>
2023-09-05 15:11:06 +0100

    new algortihm

--- internal/app/services/string_generator.go
+++ internal/app/services/string_generator.go
@@ -14,5 +14,5 @@ func NewStringGenerator() *StringGenerator {
 }
 
 func (sg *StringGenerator) GenerateRandom() string {
-	return util.Base62Encode(rand.Uint64())[:5] + util.Base62Encode(rand.Uint64())[:5]
+	return util.Base62Encode(rand.Uint64())[:6] + util.Base62Encode(rand.Uint64())[:6]
 }
