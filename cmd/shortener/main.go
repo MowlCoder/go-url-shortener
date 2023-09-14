d2e1662507256da8558aa77418ca5c9bdc4bdd7a
MowlCoder <maikezseller@gmail.com>
2023-09-14 20:20:03 +0100

    remove godotenv

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"compress/gzip"
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
-
 	_ "github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/MowlCoder/go-url-shortener/internal/config"
@@ -24,11 +21,6 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Println("No .env provided")
-	}
 
 	appConfig := &config.AppConfig{}
 	appConfig.ParseFlags()
