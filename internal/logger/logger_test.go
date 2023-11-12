74b091e3e6f2ef58ea19df73e33a7418a28d2653
MowlCoder <maikezseller@gmail.com>
2023-11-12 18:51:24 +0000

    add more tests

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -17,6 +17,16 @@ func TestNewLogger(t *testing.T) {
 		require.NotNil(t, l)
 	})
 
+	t.Run("Test new logger (success, production)", func(t *testing.T) {
+		l, err := NewLogger(Options{
+			Level:        LogInfo,
+			IsProduction: true,
+		})
+
+		require.NoError(t, err)
+		require.NotNil(t, l)
+	})
+
 	t.Run("Test new logger (fail)", func(t *testing.T) {
 		l, err := NewLogger(Options{
 			Level: "random",
@@ -25,4 +35,14 @@ func TestNewLogger(t *testing.T) {
 		require.Error(t, err)
 		require.Nil(t, l)
 	})
+}
+
+func TestLogger_Info(t *testing.T) {
+	l, err := NewLogger(Options{
+		Level:        LogInfo,
+		IsProduction: false,
+	})
+	require.NoError(t, err)
+
+	l.Info("test message")
 }
