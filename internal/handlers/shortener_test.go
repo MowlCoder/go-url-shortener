e100241053f23b10e68a52529cbc4fa715053d62
Gleb <maikezseller@gmail.com>
2023-11-15 14:59:38 +0000

    add example

--- internal/handlers/shortener_test.go
+++ internal/handlers/shortener_test.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -17,9 +16,6 @@ import (
 
 	"github.com/MowlCoder/go-url-shortener/internal/domain"
 	handlersmock "github.com/MowlCoder/go-url-shortener/internal/handlers/mocks"
-	"github.com/MowlCoder/go-url-shortener/internal/logger"
-	"github.com/MowlCoder/go-url-shortener/internal/services"
-	"github.com/MowlCoder/go-url-shortener/internal/storage"
 	"github.com/MowlCoder/go-url-shortener/internal/storage/models"
 
 	"github.com/stretchr/testify/assert"
@@ -786,47 +782,4 @@ func TestPing(t *testing.T) {
 			assert.Equal(t, testCase.ExpectedStatusCode, res.StatusCode)
 		})
 	}
-}
-
-func Example() {
-	// Initialize all dependencies for handler
-	appConfig := &config.AppConfig{
-		BaseHTTPAddr:     ":8080",
-		BaseShortURLAddr: "http://localhost:8080",
-		AppEnvironment:   config.AppDevEnv,
-	}
-	urlStorage, _ := storage.New(appConfig)
-	strGeneratorService := services.NewStringGenerator()
-	customLogger, _ := logger.NewLogger(logger.Options{
-		Level:        logger.LogInfo,
-		IsProduction: appConfig.AppEnvironment == config.AppProductionEnv,
-	})
-	queue := services.NewDeleteURLQueue(urlStorage, customLogger, 3)
-
-	// Create handler
-	handler := NewShortenerHandler(
-		appConfig,
-		urlStorage,
-		strGeneratorService,
-		queue,
-	)
-
-	// Short url
-	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://test.url"))
-	w := httptest.NewRecorder()
-	handler.ShortURL(w, r)
-	response := w.Result()
-	defer response.Body.Close()
-
-	shortURL, _ := io.ReadAll(response.Body)
-	fmt.Println("Short URL:", shortURL)
-
-	// Send GET request to shortened url and check if location header is set to original url
-	redirectRequest := httptest.NewRequest(http.MethodGet, string(shortURL), nil)
-	w = httptest.NewRecorder()
-	handler.RedirectToURLByID(w, redirectRequest)
-	response = w.Result()
-	defer response.Body.Close()
-
-	fmt.Printf("Status code: %d\nLocation: %s\n", response.StatusCode, response.Header.Get("Location"))
 }
